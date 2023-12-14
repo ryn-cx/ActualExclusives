aabea24691d0075be3215e2e644cbf703990c9b4
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-13 23:49:55 -0800

    Basic admin

--- ActualExclusives/games/admin.py
+++ ActualExclusives/games/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from games.models import Country, Game, GameGenre, GamePlatform, GamePlatformCountry, Genre, LastScrape, Platform
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(GamePlatform)
+admin.site.register(GamePlatformCountry)
+admin.site.register(GameGenre)
+admin.site.register(Genre)
+admin.site.register(Platform)
+admin.site.register(Country)
+admin.site.register(LastScrape)
