9cdaabbd37b68dbac96ac0094cc130831a80f3d5
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-25 04:55:03 -0800

    Linted

--- ActualExclusives/_activate_django.py
+++ ActualExclusives/_activate_django.py
@@ -1,10 +1,11 @@
-"""This module modifies environmental variables so that Django models can be accessed outside of Django itself.
+"""Modifies environmental variables so that Django models can be accessed outside of Django itself.
 
 When using this module to be imported before any Django models are imported. With it being located in the root directory
 and starting with an underscore isort should automatically make it import before any Django models.
 
 I hate how sketchy this is, but it is the easiest solution for consistently setting up Django with minimal issues from
-linters"""
+linters
+"""  # noqa: INP001 - This is not a package
 
 from __future__ import annotations
 
